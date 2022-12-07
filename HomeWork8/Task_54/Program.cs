31bb93b8e0a6217a6c04e7f0d11f61c0b9e4f67b
Denis <mironov_dens@mail.ru>
2022-12-07 20:58:42 +0900

    Add HomeWork8 / Task_54

---
+++ HomeWork8/Task_54/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 54. Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void Task54()
+{
+    int rows = 3;
+    int columns = 4;
+
+    int[,] matrix = new int[rows, columns];
+    FillMatrix(matrix, -50, 50);
+    PrintMatrix(matrix);
+    SortRowsToMin(matrix);
+    PrintMatrix(matrix);
+}
+
+void FillMatrix(int[,] matrix, int minValue, int maxValue)
+{
+    maxValue++;
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortRowsToMin(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        int temp = 0;
+        for (int j = 0; j < columns; j++)
+        {
+            for (int x = j + 1; x < columns; x++)
+            {
+                if (matrix[i, j] > matrix[i, x])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, x];
+                    matrix[i, x] = temp;
+                }
+            }
+        }
+    }
+    Console.WriteLine();
+}
+
+Task54();
